13d5c2b39d94d7a27b16b2446de90e1ec4202065
wangdunwei <ayvytr@163.com>
2022-08-24 10:33:21 +0800

    1.修改问题：Unsupported class file major version 60 2.删除.idea版本控制

--- calendarview/src/main/java/com/haibin/calendarview/BaseView.java
+++ calendarview/src/main/java/com/haibin/calendarview/BaseView.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 huanghaibin_dev <huanghaibin_dev@163.com>
- * WebSite https://github.com/MiracleTimes-Dev
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.haibin.calendarview;
 
 
@@ -24,9 +10,9 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
 import java.util.List;
+
+import androidx.annotation.Nullable;
 
 /**
  * 基本的日历View，派生出MonthView 和 WeekView
